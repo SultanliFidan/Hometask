168bdf00a79e9f186031f015f7ef05ba3576c951
SultanliFidan <fidanrs-ap117@code.edu.az>
2024-10-14 14:38:39 -0700

    Added ClassInheritance

---
+++ Classİnheritance/Classİnheritance/Product.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+namespace Classİnheritance
+{
+    internal class Product
+    {
+        public string Name;
+        public string BrandName;
+        public double Price;
+
+
+        public Product(string Name, string BrandName, double Price)
+        {
+            this.Name = Name;
+            this.BrandName = BrandName;
+            this.Price = Price;
+        }
+
+
+    }
+}
---
+++ Classİnheritance/Classİnheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Classİnheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] products = new Product[]
+            {
+            new Product("Hp", "Brand1", 1200),
+            new Product("Apple", "Brand3", 2000)
+            };
+
+            Product(products, 800, 2000);
+        }
+
+        static void Product(Product[] arr, double minPrice, double maxPrice)
+        {
+
+            foreach (Product product in arr)
+            {
+                if (product.Price >= minPrice && product.Price <= maxPrice)
+                {
+                    Console.WriteLine($"Name: {product.Name}, Price: {product.Price}");
+                }
+            }
+
+
+
+
+        }
+
+    }
+
+
+
+
+    
+}
+
