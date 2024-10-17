8deecd50d3b4bee99cff2e20cfd648f3a59ab109
SultanliFidan <fidanrs-ap117@code.edu.az>
2024-10-17 14:25:56 -0700

    Added Dll

---
+++ Dll/Dll/Program.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+
+namespace ModelsDll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Name = "Fidan";
+            employee.Salary = 1000;
+            employee.IsSuccessfull = true;
+
+            Assistant assistant = new Assistant();
+            assistant.GetFeedBack(employee);
+        }
+    }
+}
---
+++ Dll/Models/Class1.cs
@@ -0,0 +1,76 @@
+﻿namespace Models
+{
+    public class Manager
+    {
+        protected Employee GetPromotion(Employee employee)
+        {
+            employee.Salary += 100;
+            Console.WriteLine($"Yeni maas {employee.Salary}");
+            return employee;
+        }
+    }
+
+    public class Assistant : Manager
+    {
+        public void GetFeedBack(Employee employee)
+        {
+            if (employee.IsSuccessfull)
+            {
+                GetPromotion(employee);
+            }
+            else
+            {
+                Console.WriteLine("Ugursuzdur");
+            }
+        }
+    }
+
+    public class Employee
+    {
+        private string _name;
+        private bool _isSuccessfull;
+        private float _salary;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public bool IsSuccessfull
+        {
+            get
+            {
+                return _isSuccessfull;
+            }
+            set
+            {
+                _isSuccessfull = value;
+            }
+        }
+        public float Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value >= 350)
+                {
+                    _salary = value;
+                }
+
+            }
+        }
+
+
+    }
+
+}
