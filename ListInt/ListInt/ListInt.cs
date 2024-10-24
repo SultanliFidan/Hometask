d3c26980c3b73608fc17f2571544afd930334d87
SultanliFidan <fidanrs-ap117@code.edu.az>
2024-10-23 17:38:55 -0700

    added ListInt

---
+++ ListInt/ListInt/ListInt.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListInt
+{
+     class ListInt
+    {
+        private int[] _nums = new int[0]; 
+        public int this[int index]
+        {
+            get
+            {
+                return _nums[index];
+            }
+            set
+            {
+                _nums[index] = value;
+            }
+        }
+
+
+        public void Add(int num)
+        {
+            Array.Resize(ref _nums, _nums.Length + 1);
+            _nums[_nums.Length - 1] = num;
+        }
+
+        public void Add(params int[] nums)
+        {
+            int index = _nums.Length;
+            Array.Resize(ref _nums, _nums.Length + nums.Length);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                _nums[index+i] = nums[i];
+            }
+        }
+
+        public bool Contains(int num)
+        {
+            foreach (int i in _nums)
+            {
+               if (i == num)
+                {
+
+                  return true;
+                }
+            }
+            return false;
+        }
+
+        public int Pop()
+        {
+            int num = _nums[_nums.Length - 1];
+            Array.Resize(ref _nums,_nums.Length - 1);
+            return num;
+        }
+
+        public int Sum()
+        {
+            int sum = 0;
+            for(int i = 0; i < _nums.Length; i++)
+            {
+                sum += _nums[i];
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+
+            StringBuilder sb = new StringBuilder();
+            foreach(int i in _nums)
+            {
+                sb.Append(i.ToString());
+                sb.Append(" ");
+            }
+            return sb.ToString();
+        }
+
+        public int IndexOf(int num)
+        {
+            for (int i = 0; i < _nums.Length; i++)
+            {
+                if (_nums[i] == num)
+                {
+                    return i; 
+                }
+            }
+            return -1; 
+        }
+
+        public int LastIndexOf(int num)
+        {
+            for (int i = _nums.Length - 1; i >= 0; i--)
+            {
+                if (_nums[i] == num)
+                {
+                    return i; 
+                }
+            }
+            return -1; 
+        }
+
+        public void Insert(int num,int index)
+        {
+            int[] array = new int[_nums.Length + 1];
+            for(int i = 0; i <index; i++)
+            {
+                array[i] = _nums[i];
+            }
+            array[index] = num;
+            for(int i = index; i < _nums.Length; i++)
+            {
+                array[i+ 1] = _nums[i];
+            }
+            _nums = array;
+        }
+
+
+        public float Average()
+        {
+            float sum = Sum();
+            if (_nums.Length == 0)
+            {
+                Console.WriteLine("Sifira bolmek olmur");
+                return -1;
+            }
+            return sum/ _nums.Length; 
+        }
+    }
+}
---
+++ ListInt/ListInt/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ListInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListInt listInt = new ListInt();
+
+           
+            listInt.Add(1);
+            listInt.Add(2);
+            listInt.Add(3);
+            listInt.Add(4, 5, 6);
+
+            
+            Console.WriteLine(listInt);
+
+        
+            Console.WriteLine(listInt.Contains(3));
+
+          
+            Console.WriteLine(listInt.Pop());
+
+
+            Console.WriteLine( listInt.IndexOf(4));
+
+            Console.WriteLine(listInt.LastIndexOf(6));
+            Console.WriteLine( listInt.Average());
+
+
+            listInt.Insert(99, 2); 
+            Console.WriteLine(listInt);
+
+        }
+    }
+}
