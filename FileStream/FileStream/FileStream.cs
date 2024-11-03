30f37b12e5d79107a775655901e3115c5ded92b5
SultanliFidan <fidanrs-ap117@code.edu.az>
2024-11-03 13:17:01 -0800

    FileStream added:

---
+++ FileStream/FileStream/FileStream.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FileStream
+{
+    internal class FileStream
+    {
+
+        public string Path { get; }
+        public FileStream(string path)
+        {
+            Path = path;
+        }
+
+        List<string> names = [];
+
+        public void Add(string name)
+        {
+
+            
+            names.Add(name);
+            string result = JsonConvert.SerializeObject(names);
+            using StreamWriter sw = new StreamWriter(Path);
+            sw.WriteLine(result);
+        }
+
+        public bool Exist(string name)
+        {
+            bool isExist = false;
+            using StreamReader sr = new StreamReader(Path);
+            string result = sr.ReadToEnd();
+            var objname = JsonConvert.DeserializeObject<List<string>>(result);
+            if (objname.Contains(name))
+            {
+                isExist = true;
+            }
+            return isExist;
+
+        }
+
+        public void Update(int index, string name)
+        {
+            if (index >= 0 && index < names.Count)
+            {
+                
+                    names[index] = name;
+                
+                string result = JsonConvert.SerializeObject(names);
+                using StreamWriter sw = new StreamWriter(Path);
+                sw.WriteLine(result);
+            }
+
+        }
+        public void Delete(int index)
+        {
+            if (index >= 0 && index < names.Count)
+            {
+                
+                    names.RemoveAt(index);
+                
+                string result = JsonConvert.SerializeObject(names);
+                using StreamWriter sw = new StreamWriter(Path);
+                sw.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ FileStream/FileStream/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FileStream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(@"C:\Users\User\Desktop\Hometask\FileStream\FileStream\Files\names.json");
+            FileStream file = new FileStream(path);
+            file.Add("Fidan");
+            file.Add("Jale");
+
+            Console.WriteLine(file.Exist("Fidan"));
+            //file.Update(1, "Nezrin");
+            //file.Delete(1);
+
+        }
+    }
+}
